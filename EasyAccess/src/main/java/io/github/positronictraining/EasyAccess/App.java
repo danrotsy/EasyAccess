e1a6c49568854940ceddb8c8ad22d5808f83f39d
pawnPhilosopher <dvr@fastmail.fm>
2014-09-23 15:10:00 -0400

    Initial commit of Easy Access

---
+++ EasyAccess/src/main/java/io/github/positronictraining/EasyAccess/App.java
@@ -0,0 +1,13 @@
+package io.github.positronictraining.EasyAccess;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
